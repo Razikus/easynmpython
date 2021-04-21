6cf13fe4b02dfc00ad6878924fe870eaef0f97b2
adam <adam@razniewski.eu>
2021-04-21 12:02:33 +0200

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="easynmpython",
-    version="1.0.0",
+    version="1.0.2",
     author="Adam Raźniewski",
     author_email="adam@razniewski.eu",
     description="Easy NetworkManager DBUS",
